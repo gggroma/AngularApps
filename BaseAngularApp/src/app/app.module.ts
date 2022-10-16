198a5b6fcc15e3c4101d758eeaeee40dddadc50c
Roman Gusar <romang@bocada.com>
2022-10-16 22:18:45 +0200

    Post components added

--- BaseAngularApp/src/app/app.module.ts
+++ BaseAngularApp/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
+import { Post2Component } from './post2/post2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    Post2Component
   ],
   imports: [
     BrowserModule,
