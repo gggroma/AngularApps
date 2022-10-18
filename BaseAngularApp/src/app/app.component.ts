8a6504057e2250c93b3d4aebe8c6b9eb9d756699
Roman Gusar <romang@bocada.com>
2022-10-18 13:17:30 +0200

    minor changes

--- BaseAngularApp/src/app/app.component.ts
+++ BaseAngularApp/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'BaseAngularApp';
+  title = 'Dynamic Title';
+  number = 42
 }
